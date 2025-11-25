428a590660dd20c1f1a2f077b3f0edc9f36bda8d
NasrWahi <nasr.wahi@gmail.com>
2025-11-25 18:08:02 +0100

    "Week 3 Finished Exercises Tasks"

--- week3_query_weatherdata/sql/analyze_weather.sql
+++ week3_query_weatherdata/sql/analyze_weather.sql
@@ -44,11 +44,52 @@ ORDER BY "Country/Region", "Province/State";
 /* ==========
     Task 4
    ========== */
-
 SELECT
   to_timestamp(sunriseTime) AS sunrise_utc, -- this function transfers numeric columns to timestamp
   to_timestamp(sunriseTime) AT TIME ZONE 'Europe/Stockholm' AS sunrise_swtime,
   to_timestamp(sunsetTime) AS sunset_utc,
   to_timestamp(sunsetTime) AT TIME ZONE 'Europe/Stockholm' AS sunset_swtime,
 FROM staging.daily_weather_2020
-WHERE "Country/Region" = 'Sweden'; -- nnote the use of single and double quotations
+WHERE "Country/Region" = 'Sweden'; -- note the use of single and double quotations
+
+/* ==========
+    Task 5
+   ========== */
+   
+-- the new year and month columns involves subtracting a part of timestamp
+-- to pick up the date with the largest gap within a month involves the use of aggregation function
+-- the gaps can be calculated directly with UNIX time
+SELECT 
+  date_part('year', to_timestamp(sunriseTime)) AS year,
+  date_part('month', to_timestamp(sunriseTime)) AS month,
+  MAX(to_timestamp(sunriseTime)) AS sunrise_largest_gap_date, -- MAX() will pick up the first row
+  MAX(to_timestamp(sunsetTime)) AS sunset_largest_gap_date, -- MAX() will pick up the first row
+  MAX(ROUND((sunsetTime-sunriseTime)/3600)) AS gap_hours,
+FROM staging.daily_weather_2020
+WHERE "Country/Region" = 'Sweden'
+GROUP BY year, month
+ORDER BY year, month, gap_hours DESC; -- make sure the date with the largest gap is always the first row for each year-month
+/* ==========
+    Task 6
+   ========== */
+
+-- concatenate integer and string
+SELECT
+  to_timestamp(windGustTime) AT TIME ZONE 'Europe/Stockholm' AS most_windy_timestamp,
+  date_part('hour', most_windy_timestamp) AS most_windy_hour,
+  CONCAT('It''s dangerous to use the crane at kl.', most_windy_hour)
+FROM staging.daily_weather_2020
+WHERE "Country/Region" = 'Sweden';
+-- ^ alternative 1
+
+-- concatenate string and string
+SELECT
+  to_timestamp(windGustTime) AT TIME ZONE 'Europe/Stockholm' AS most_windy_timestamp,
+  -- strftime(), string format time, transforms timestamp to string
+  -- use the format, like '%H', to design the presentation
+  -- strptime() , string past time, transforms string to timestamp
+  strftime(most_windy_timestamp,'%H') AS most_windy_hour,
+  CONCAT('It''s dangerous to use the crane at kl.', most_windy_hour)
+FROM staging.daily_weather_2020
+WHERE "Country/Region" = 'Sweden';
+-- ^ alternative 2
